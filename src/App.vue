aa856117368efa565b140a61e1c33387d4944dac
xushuo <893804934@qq.com>
2017-03-22 14:21:26 +0800

    待办事项  demo

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,33 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <hello></hello>
+    <h2>{{title}}</h2>
+    <input v-model="newItem" @keyup.enter="addView">
+    <ul>
+      <li v-for="item in items" v-bind:class="{finish:item.isFinished}" v-on:click="toggleFinish(item)" >{{item.label}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
-
 export default {
-  name: 'app',
-  components: {
-    Hello
+   data(){
+     return {
+       title:'待办事项',
+       items:[],
+       newItem:''
+     }
+   },
+   methods:{
+     toggleFinish(item){
+       item.isFinished =!item.isFinished;
+     },
+     addView(){
+       if(this.newItem!=''){
+         this.items.push({label:this.newItem,isFinished:false});
+       }
+       this.newItem ='';
+     }
   }
 }
 </script>
@@ -24,5 +40,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  width: 500px;
+  margin: 0 auto;
 }
+  .finish{
+    text-decoration: line-through;
+  }
 </style>
